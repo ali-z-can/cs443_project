d5a71ef201f60d4bcf38bffd56af04429b0bd0c7
alican <can.zeybek@ug.bilkent.edu.tr>
2020-04-01 18:27:20 +0300

    implemented naming server and config server

--- netflixeurekanaimgserver/src/main/java/com/cs443/project/netflixeurekanaimgserver/NetflixeurekanaimgserverApplication.java
+++ netflixeurekanaimgserver/src/main/java/com/cs443/project/netflixeurekanaimgserver/NetflixeurekanaimgserverApplication.java
@@ -2,7 +2,9 @@ package com.cs443.project.netflixeurekanaimgserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class NetflixeurekanaimgserverApplication {
 
