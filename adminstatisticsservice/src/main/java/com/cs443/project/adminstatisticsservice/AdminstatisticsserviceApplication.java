5056d98f5fec10e21028f217f90d1c4a5fd69841
alican <can.zeybek@ug.bilkent.edu.tr>
2020-04-28 17:08:03 +0300

    added df conf

--- adminstatisticsservice/src/main/java/com/cs443/project/adminstatisticsservice/AdminstatisticsserviceApplication.java
+++ adminstatisticsservice/src/main/java/com/cs443/project/adminstatisticsservice/AdminstatisticsserviceApplication.java
@@ -2,11 +2,13 @@ package com.cs443.project.adminstatisticsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@EnableFeignClients("com.cs443.project.adminstatisticsservice")
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class AdminstatisticsserviceApplication {
 
--- adminstatisticsservice/src/main/java/com/cs443/project/adminstatisticsservice/UserAnalyticsProxy.java
+++ adminstatisticsservice/src/main/java/com/cs443/project/adminstatisticsservice/UserAnalyticsProxy.java
@@ -1,15 +1,13 @@
 package com.cs443.project.adminstatisticsservice;
 
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="inner-gateway")
-@RibbonClient(name = "user-past-link-analytics-retrieval-service")
+@FeignClient(name="userpastlinkanalyticsretrievalservice")
 public interface UserAnalyticsProxy {
 
-    @GetMapping("/user-past-link-analytics-retrieval-service/admin/getUserStats")
+    @GetMapping("/admin/getUserStats")
     public ToSendUser getUserStats();
 
 }
