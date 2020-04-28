309c68744f46754060660e132086825b9657a732
43759855 <burak.kirimli35@gmail.com>
2020-04-29 02:14:01 +0300

    updated userpastlinkanalyticsarrangementservice

--- userpastlinkanalyticsarrangementservice/src/main/java/com/cs443/project/userpastlinkanalyticsarrangementservice/PastLinkRearrController.java
+++ userpastlinkanalyticsarrangementservice/src/main/java/com/cs443/project/userpastlinkanalyticsarrangementservice/PastLinkRearrController.java
@@ -1,6 +1,7 @@
 package com.cs443.project.userpastlinkanalyticsarrangementservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class PastLinkRearrController {
     UserRepository userRepository;
 
 
+    @CrossOrigin
     @GetMapping("/rearrangement/user/{userID}/numberOfClicks/{numberOfClicks}")
     public boolean rearrange(@PathVariable Long userID,@PathVariable int numberOfClicks){
 
--- userpastlinkanalyticsarrangementservice/src/main/java/com/cs443/project/userpastlinkanalyticsarrangementservice/UserpastlinkanalyticsarrangementserviceApplication.java
+++ userpastlinkanalyticsarrangementservice/src/main/java/com/cs443/project/userpastlinkanalyticsarrangementservice/UserpastlinkanalyticsarrangementserviceApplication.java
@@ -1,10 +1,9 @@
 package com.cs443.project.userpastlinkanalyticsarrangementservice;
 
-import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -13,7 +12,5 @@ public class UserpastlinkanalyticsarrangementserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserpastlinkanalyticsarrangementserviceApplication.class, args);
 	}
-	@Bean
-	public Sampler defaultSampler(){return Sampler.ALWAYS_SAMPLE;}
 
 }
